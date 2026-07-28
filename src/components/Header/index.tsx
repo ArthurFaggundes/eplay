fffa1e5b3cd484814d7650b8d93d61933772dd42
Arthur Faggundês <arthurhfagundes6@gmail.com>
2026-07-28 17:22:34 -0300

    [ feat ] added Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import { HeaderBar, Links, LinkItem, LinkCart } from './styles'
+
+import logo from '../../assets/images/logo.svg'
+import cart from '../../assets/images/cart.svg'
+
+const Header = () => (
+  <HeaderBar>
+    <div>
+      <img src={logo} alt="EPLAY" />
+      <nav>
+        <Links>
+          <LinkItem>
+            <a href="#">Categories</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">New Games</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Offers</a>
+          </LinkItem>
+        </Links>
+      </nav>
+    </div>
+    <LinkCart href="#">
+      0 Product(s)
+      <img src={cart} alt="Shopping Cart" />
+    </LinkCart>
+  </HeaderBar>
+)
+
+export default Header
