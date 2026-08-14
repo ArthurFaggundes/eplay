adf7472fdd95e33d36acad14e568e790d9e64ca8
Arthur Faggundês <arthurhfagundes6@gmail.com>
2026-08-14 17:15:27 -0300

    [ feat ] add responsive breaks and update styles  >> - Gallery, - Header, - Hero, and - ProductList components

--- src/components/ProductList/styles.ts
+++ src/components/ProductList/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 import { Props } from '.'
-import { colors } from '../../styles'
+import { colors, breakpoints } from '../../styles'
 import { Card } from '../Product/styles'
 
 export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
@@ -17,9 +17,17 @@ export const Container = styled.section<Omit<Props, 'title' | 'games'>>`
 
 export const List = styled.ul`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-columns: 1fr 1fr 1fr 1fr; //* [] [] [] []
   gap: 24px;
   margin-top: 40px;
+
+  @media (max-width: ${breakpoints.desktop}) {
+    grid-template-columns: 1fr 1fr; //* [] []
+  }
+
+  @media (max-width: ${breakpoints.tablet}) {
+    grid-template-columns: 1fr; //* []
+  }
 `
 
 export const Title = styled.h2`
