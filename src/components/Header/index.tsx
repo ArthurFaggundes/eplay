fa3dc7b7880855d0cbc6500c7ae7f35a048cd75a
Arthur Faggundês <arthurhfagundes6@gmail.com>
2026-08-17 16:50:23 -0300

    [ feat ] refactor Header component layout and styles for improved mobile navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,19 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
-import { RootReducer } from '../../store'
 
+import { RootReducer } from '../../store'
 import { open } from '../../store/reducers/cart'
 
-import { HeaderBar, Links, LinkItem, CartButton } from './styles'
+import {
+  HeaderBar,
+  Links,
+  LinkItem,
+  CartButton,
+  Hamburguer,
+  HeaderRow,
+  NavMobile
+} from './styles'
 
 import logo from '../../assets/images/logo.svg'
 import cart from '../../assets/images/cart.svg'
@@ -12,6 +21,7 @@ import cart from '../../assets/images/cart.svg'
 const Header = () => {
   const dispatch = useDispatch()
   const { items } = useSelector((state: RootReducer) => state.cart)
+  const [isMobileNavOpen, setIsMobileNavOpen] = useState(false)
 
   const openCart = () => {
     dispatch(open())
@@ -19,28 +29,48 @@ const Header = () => {
 
   return (
     <HeaderBar>
-      <div>
-        <Link to="/">
-          <img src={logo} alt="EPLAY" />
-        </Link>
-        <nav>
-          <Links>
-            <LinkItem>
-              <Link to="/categories">Categories</Link>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">New Games</a>
-            </LinkItem>
-            <LinkItem>
-              <a href="#">Offers</a>
-            </LinkItem>
-          </Links>
-        </nav>
-      </div>
-      <CartButton onClick={openCart}>
-        {items.length} Product(s)
-        <img src={cart} alt="Shopping Cart" />
-      </CartButton>
+      <HeaderRow>
+        <div>
+          <Hamburguer onClick={() => setIsMobileNavOpen(!isMobileNavOpen)}>
+            <span />
+            <span />
+            <span />
+          </Hamburguer>
+          <Link to="/">
+            <img src={logo} alt="EPLAY" />
+          </Link>
+          <nav>
+            <Links>
+              <LinkItem>
+                <Link to="/categories">Categories</Link>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">New Games</a>
+              </LinkItem>
+              <LinkItem>
+                <a href="#">Offers</a>
+              </LinkItem>
+            </Links>
+          </nav>
+        </div>
+        <CartButton onClick={openCart}>
+          {items.length} <span>Product(s)</span>
+          <img src={cart} alt="Shopping Cart" />
+        </CartButton>
+      </HeaderRow>
+      <NavMobile className={isMobileNavOpen ? 'is-open' : ''}>
+        <Links>
+          <LinkItem>
+            <Link to="/categories">Categories</Link>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">New Games</a>
+          </LinkItem>
+          <LinkItem>
+            <a href="#">Offers</a>
+          </LinkItem>
+        </Links>
+      </NavMobile>
     </HeaderBar>
   )
 }
